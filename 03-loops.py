2356ca2229bfa8ef58bbd128523d0867baa3d505
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-09-10 08:49:53 +0530

    Add files via upload

---
+++ 03-loops.py
@@ -0,0 +1,6 @@
+##Task
+##The provided code stub reads and integer,n , from STDIN. For all non-negative integers i<n, print i^2
+
+if __name__ == '__main__':
+    n = int(input())
+    [print(i**2) for i in range(n)]
